c43ea69a8dcd0a874a4c32761706a58db77f1a9d
Jordan Safer <jordansafer2@gmail.com>
2020-05-12 02:00:34 +0000

    upload warrior file no hashes

--- index.js
+++ index.js
@@ -1,74 +1,37 @@
 const fs = require("fs");
-const crypto = require("crypto");
-
 const upload = require("./tools/upload.js");
 
-const hash_file = "code_hashes.json";
 
 /**
  * Change this for files to save 
  */
 const save_map = {
-    "code/test.js": {slot: 60, name: "test"},
-    "code/test2.js": {slot: 61, name: "test2"}
+    "code/warrior.js": {slot: 2, name: "warrior"}
 };
-
-
-/**
- * Check checksum of files to avoid uploading unchanged files
- */
-function checksum(file_path) {
-    const file_content = fs.readFileSync(file_path).toString();
-    return crypto.createHash("md5").update(file_content, 'utf8').digest("hex");
-}
-
 
 // Function to check file checksums and upload if it's changed
 async function upload_changed_files() {
-    // Get the last hashes
-    let code_hashes = {};
-    if (fs.existsSync(hash_file)) {
-        code_hashes = JSON.parse(fs.readFileSync(hash_file));
-    }
-
-    let found_change = false;
-
     // Upload files that has changed
     for (const file_path in save_map) {
-        const file_checksum = checksum(file_path);
-        if (code_hashes[file_path] !== file_checksum) {
-            found_change = true;
-            const slot = save_map[file_path].slot;
-            const name = save_map[file_path].name;
+        const slot = save_map[file_path].slot;
+        const name = save_map[file_path].name;
 
-            console.log(`Checksum changed in file ${file_path}`);
-            let upload_result;
-            try {
-                upload_result = await upload(file_path, slot, name);
-            } catch(error) {
-                console.log(`Error uploading ${file_path} with reason: ${JSON.stringify(reason)}`);
-                continue;
-            }
-            const json_data = await upload_result.json();
-            if (json_data[0].type === "message") {
-                console.log(`Uploaded ${file_path} to save slot ${slot} with name ${name}`);
-                console.log(`${json_data[0].message}`);
-
-                // Store the new checksum
-                code_hashes[file_path] = file_checksum;
-            } else {
-                console.log(`Error during upload. Server gave responded with a ${json_data[0].type}`);
-                console.log(`${json_data[0].message}`);
-            }
+        let upload_result;
+        try {
+            upload_result = await upload(file_path, slot, name);
+        } catch(error) {
+            console.log(`Error uploading ${file_path} with reason: ${JSON.stringify(error.message)}`);
+            continue;
+        }
+        const json_data = await upload_result.json();
+        if (json_data[0].type === "message") {
+            console.log(`Uploaded ${file_path} to save slot ${slot} with name ${name}`);
+            console.log(`${json_data[0].message}`);
+        } else {
+            console.log(`Error during upload. Server gave responded with a ${json_data[0].type}`);
+            console.log(`${json_data[0].message}`);
         }
     }
-
-    if (!found_change) {
-        console.log("No changes found in the specified files");
-    }
-
-    // Store new checksum hashes
-    fs.writeFileSync(hash_file, JSON.stringify(code_hashes));
 }
 
 upload_changed_files();
