4845648afb38601490d6d21b24667d3b35cfafd1
Jordan Safer <jordansafer2@gmail.com>
2020-05-12 02:49:41 +0000

    smart scoring based on xp and health

--- code/warrior.js
+++ code/warrior.js
@@ -1,7 +1,3 @@
-// Hey there!
-// This is CODE, lets you control your character with code.
-// If you don't know how to code, don't worry, It's easy.
-// Just set attack_mode to true and ENGAGE!
 
 const attack_mode=true
 const second = 1000
@@ -11,8 +7,15 @@ setInterval(function() {
     respawn()
 }, 20 * second)
 
+// change target if target is dead
+setInterval(function() {
+    const target = get_targeted_monster()
+    if (!target || target.rip) {
+        smarter_move()
+    }
+}, 2 * second)
 
-
+// find a target, attack it
 setInterval(function(){
     use_hp_or_mp()
     loot()
@@ -22,7 +25,7 @@ setInterval(function(){
     let target=get_targeted_monster()
     if(!target)
     {
-        target=get_nearest_monster({min_xp:450,max_att:250,max_hp:10000})
+        target=get_monster_for_attack()
         game_log(target && target.mtype)
         if(target) change_target(target)
         else {
@@ -32,19 +35,12 @@ setInterval(function(){
     }
 
     // escape don't die
-    if(character.hp < 0.05 * character.max_hp) {
+    if(character.hp < 0.1 * character.max_hp) {
         use("use_town")
     }
-
-
     if(!is_in_range(target))
     {
-        smart_move(target)
-        //move(
-        //	(character.x+2*target.x)/3,
-        //	(character.y+2*target.y)/3
-        //	);
-        // Walk half the distance
+        smarter_move(target)
     }
     else if(can_attack(target))
     {
@@ -76,43 +72,82 @@ function use_hp_or_mp_regen()
     if(new Date()<parent.next_skill.use_hp) return
     if(new Date()<parent.next_skill.use_mp) return
     if(character.mp/character.max_mp<0.9) use("regen_mp"), used_regen=true
-    else if(character.hp/character.max_hp<0.7) use("regen_hp"), used_regen=true
+    else if(character.hp/character.max_hp<0.9) use("regen_hp"), used_regen=true
 
     if(used_regen) last_regen=new Date()
 }
 
-//Source code of: get_nearest_monster
-function get_nearest_monster(args)
-{
-    //args:
-    // max_att - max attack
-    // min_xp - min XP
-    // target: Only return monsters that target this "name" or player object
-    // no_target: Only pick monsters that don't have any target
-    // path_check: Checks if the character can move to the target
-    // type: Type of the monsters, for example "goo", can be referenced from `show_json(G.monsters)` [08/02/17]
-    var min_d=999999,target=null
-
-    if(!args) args={}
-    if(args && args.target && args.target.name) args.target=args.target.name
-    if(args && args.type=="monster") game_log("get_nearest_monster: you used monster.type, which is always 'monster', use monster.mtype instead")
-    if(args && args.mtype) game_log("get_nearest_monster: you used 'mtype', you should use 'type'")
-
+function get_monster_for_attack() {
+    // const min_rate = 0.5
+    const max_hp = 10000
+    const max_att = character.attack
+	
     const sprites = Object.values(parent.entities)
-    const sortedSprites = sprites.sort((a,b) => a.xp > b.xp).reverse()
-
-    for (const current of sortedSprites) {
-        if(current.type != "monster" || !current.visible || current.dead) continue
-        if(args.type && current.mtype!=args.type) continue
-        if(args.max_hp && current.hp>args.max_hp) continue
-        //if(args.min_xp && current.xp<args.min_xp) continue
-        //game_log(`${current.mtype}: ${current.attack}`)
-        if(args.max_att && current.attack>args.max_att) continue
-        if(args.target && current.target!=args.target) continue
-        if(args.no_target && current.target && current.target!=character.name) continue
-        if(args.path_check && !can_move_to(current)) continue
-        var c_dist=parent.distance(character,current)
-        if(c_dist<min_d) min_d=c_dist,target=current
+    const monsters = sprites.filter(sp => sp.type == "monster")
+    const chars = sprites.filter(sp => sp.type == "character")
+	
+    const sortedMonsters = monsters.sort(monster_cmp).reverse()
+	
+    for (const monster of sortedMonsters) {
+        if (monster.dead) continue
+        if (monster.max_hp > max_hp || monster.attack > max_att) continue
+        if (chars.find(char => char_to_close(monster, char))) continue
+        return monster
     }
-    return target
+    return 0 // none found
+}
+
+function char_to_close(monster, char) {
+    return (char.rip == "false") && in_range_to(monster, char, 500)
+}
+
+function monster_cmp(m1, m2) {
+    const r1 = get_xp_rate(m1)
+    const r2 = get_xp_rate(m2)
+    const range1 = get_range(character, m1) / 500
+    const range2 = get_range(character, m2) / 500
+    return r1 - range1 > r2 - range2
+}
+
+// xp per hp. good rates are over
+// Using max_hp since other player damage could distort calculation
+// TODO add dodging ability
+function get_xp_rate(monster) {
+    const trueHp = monster.max_hp
+    return monster.xp / trueHp
+}
+
+function get_range(a, b) {
+    const dx = a.x - b.x
+    const dy = a.y - b.y
+    return Math.sqrt(dx**2 + dy**2)
+}
+	
+function in_range_to(a, b, range) {
+    return get_range(a, b) < range
+}
+
+function smarter_move(destination)
+{
+    if (!destination) {
+        smart.moving = false
+        return
+    }
+    if(smart.moving) smart.on_done(false,"interrupted")
+    smart.map=""
+    smart.map=destination.map||character.map
+    smart.x=destination.x
+    smart.y=destination.y
+    if(!smart.map)
+    {
+        game_log("Unrecognized location","#CF5B5B")
+        return rejecting_promise({reason:"invalid"})
+    }
+    smart.moving=true
+    smart.plot=[]; smart.flags={}; smart.searching=smart.found=false
+    smart.on_done=function(done,reason){
+        if(done) resolve_deferreds("smart_move",{success:true})
+        else reject_deferreds("smart_move",{reason:reason})
+    }
+    return push_deferred("smart_move")
 }
